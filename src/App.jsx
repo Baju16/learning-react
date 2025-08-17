765752aeffbc5cbdfcba851ae2f6ac732b67282f
Baju16 <dawidbajek03g@gmail.com>
2025-08-17 14:24:52 +0200

    passing custom args in event function

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
+  function handleSelect(selectedButton){
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -19,6 +24,16 @@ function App() {
             ))}
             
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Example</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
